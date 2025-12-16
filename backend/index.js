51665bb1834928edbbf811ffca0524ee9f713176
Ignacio <josenunezm2001@gmail.com>
2025-12-16 20:54:48 -0300

    Index backend modificado para aceptar cors en localhost

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import { config } from "./src/config/config.js";
+import cors from "cors";
+
+const port = config.port;
 
 const app = express();
-const port = config.port;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, console.log("Servidor iniciado » Escuchando en puerto", port));
