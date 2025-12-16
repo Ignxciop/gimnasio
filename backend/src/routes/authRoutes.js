3c7f0c40c4ee45b69b42668f208cd8893ab4ba14
Ignacio <josenunezm2001@gmail.com>
2025-12-16 18:42:33 -0300

    Se agregó la ruta para login

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,23 @@
 import express from "express";
 import authController from "../controllers/authController.js";
-import { registerValidation, validate } from "../validators/userValidator.js";
+import {
+    registerValidation,
+    validate as validateRegister,
+} from "../validators/userValidator.js";
+import {
+    loginValidation,
+    validate as validateLogin,
+} from "../validators/authValidator.js";
 
 const router = express.Router();
 
-router.post("/register", registerValidation, validate, authController.register);
+router.post(
+    "/register",
+    registerValidation,
+    validateRegister,
+    authController.register
+);
+
+router.post("/login", loginValidation, validateLogin, authController.login);
 
 export default router;
