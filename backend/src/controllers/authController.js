dbc49bd6f32c81a42541dcf0e1a3058f489c9e0d
Ignacio <josenunezm2001@gmail.com>
2025-12-16 02:31:49 -0300

    Controlador: user

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,27 @@
+import userService from "../services/userService.js";
+
+export class AuthController {
+    async register(req, res, next) {
+        try {
+            const { name, lastname, username, email, password, roleId } =
+                req.body;
+
+            const user = await userService.createUser({
+                name,
+                lastname,
+                username,
+                email,
+                password,
+                roleId: parseInt(roleId),
+            });
+
+            res.status(201).json({
+                success: true,
+                message: "Usuario registrado exitosamente",
+                data: user,
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+}
