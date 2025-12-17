062a8bda4eb86b898fe48df50ed5026ddf8380c5
Ignacio <josenunezm2001@gmail.com>
2025-12-17 00:07:46 -0300

    feat: Middleware de autenticación en rutas y handler de errores en Index

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { config } from "./src/config/config.js";
 import cors from "cors";
+import authRoutes from "./src/routes/authRoutes.js";
+import { errorHandler } from "./src/middlewares/errorHandler.js";
 
 const port = config.port;
 
@@ -8,5 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRoutes);
+
+app.use(errorHandler);
 
 app.listen(port, console.log("Servidor iniciado » Escuchando en puerto", port));
