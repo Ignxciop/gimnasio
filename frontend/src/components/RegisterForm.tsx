e277dfcfb3c7345d81e4edad11ddc6813d1ddf41
Ignacio <josenunezm2001@gmail.com>
2025-12-27 06:04:14 -0300

    feat: Agregar mensaje de ayuda con requisitos de contraseña

--- frontend/src/components/RegisterForm.tsx
+++ frontend/src/components/RegisterForm.tsx
@@ -221,6 +221,12 @@ export const RegisterForm: React.FC<RegisterFormProps> = ({ onSuccess }) => {
                     disabled={isLoading}
                     autoComplete="new-password"
                 />
+                {!errors.password && (
+                    <span className="form__help-text">
+                        Mínimo 4 caracteres, debe incluir: mayúscula, minúscula
+                        y número
+                    </span>
+                )}
             </div>
 
             <div className="form__field">
